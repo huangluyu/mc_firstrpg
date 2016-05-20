e7fb19ceecc06edb52d24612857fb2ce3cc89ee3
huangluyu <huangluyu2012@gmail.com>
2016-05-20 21:38:10 +0800

    rpg 0.1.3

--- src/main/java/com/github/huangluyu/firstrpg/entity/EntityGoldenChicken.java
+++ src/main/java/com/github/huangluyu/firstrpg/entity/EntityGoldenChicken.java
@@ -2,18 +2,27 @@ package com.github.huangluyu.firstrpg.entity;
 
 import com.github.huangluyu.firstrpg.item.ItemLoader;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.attributes.IAttribute;
 import net.minecraft.entity.ai.attributes.RangedAttribute;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 
 public class EntityGoldenChicken extends EntityChicken
 {
-    public EntityGoldenChicken(World worldIn)
+	public EntityGoldenChicken(World worldIn)
     {
         super(worldIn);
         this.setSize(1.2F, 1.8F);
+        this.tasks.addTask(8, new AIStackBlock(this));
     }
 
     @Override
@@ -47,5 +56,87 @@ public class EntityGoldenChicken extends EntityChicken
     
     public static final IAttribute wingSpeed = new RangedAttribute(null, "firstRpg.GoldenChicken.wingSpeed", 1.5D, 0.0D,
             4.0D).setDescription("Wing Speed").setShouldWatch(true);
+    
+    //注册DataWatcher
+    @Override
+    protected void entityInit()
+    {
+        super.entityInit();
+        this.dataWatcher.addObject(16, new Byte((byte) 0));
+    }
+    
+    //数据上传
+    @Override
+    public boolean interact(EntityPlayer player)
+    {
+        if (!super.interact(player))
+        {
+            byte b = this.dataWatcher.getWatchableObjectByte(16);
+            this.dataWatcher.updateObject(16, new Byte((byte) ((b + 1) % 5)));
+        }
+        return true;
+    }
+    
+    //nbt存储
+    @Override
+    public void writeEntityToNBT(NBTTagCompound tagCompound)
+    {
+        super.writeEntityToNBT(tagCompound);
+        tagCompound.setByte("WingSpeedMultiplier", this.dataWatcher.getWatchableObjectByte(16));
+    }
 
+    @Override
+    public void readEntityFromNBT(NBTTagCompound tagCompund)
+    {
+        super.readEntityFromNBT(tagCompund);
+        this.dataWatcher.updateObject(16, tagCompund.getByte("WingSpeedMultiplier"));
+    }
+
+    //AI
+    protected static class AIStackBlock extends EntityAIBase
+    {
+        private final EntityGoldenChicken entity;
+
+        public AIStackBlock(EntityGoldenChicken entity)
+        {
+            this.entity = entity;
+        }
+
+        //AI判断
+        @Override
+        public boolean shouldExecute()
+        {
+            return this.entity.worldObj.getGameRules().getGameRuleBooleanValue("mobGriefing");
+            // Minecraft 1.8.9: this.entity.worldObj.getGameRules().getBoolean("mobGriefing");
+        }
+
+        //捡起脚下方块并放置方块
+        @Override
+        public void updateTask()
+        {
+            BlockPos entityPos = new BlockPos(entity);
+            if (entity.worldObj.isAirBlock(entityPos.up()))
+            {
+                AxisAlignedBB aabb = AxisAlignedBB.fromBounds(entityPos.getX() - 1, entityPos.getY(),
+                        entityPos.getZ() - 1, entityPos.getX() + 2, entityPos.getY() + 1, entityPos.getZ() + 2);
+                for (Object e : entity.worldObj.getEntitiesWithinAABB(EntityItem.class, aabb))
+                {
+                    ItemStack stack = ((EntityItem) e).getEntityItem();
+                    Block block = Block.getBlockFromItem(stack.getItem());
+                    if (block != null)
+                    {
+                        entity.setLocationAndAngles(entity.posX, entity.posY + 1, entity.posZ, entity.rotationYaw,
+                                entity.rotationPitch);
+                        entity.worldObj.setBlockState(entityPos, block.getDefaultState());
+                        if (--stack.stackSize <= 0)
+                        {
+                            ((EntityItem) e).setDead();
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    
 }
